18b9c2f2949a06db50e2d6f90f0e8cccae1f036a
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-12-06 21:03:48 +0100

    completata logica di base

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // creao il la variabile 'rl' fhe userò per prendere gli imput dell'utente
+const { Console } = require("console");
 const readline = require("readline")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
+
 
   //funzione che prende in ingresso un nuero e due stringhe, ritorna il numero se non è divisibile per 3 e 5, 
   //oppure ritorna la stringa(se divisibile sia per 3 che per 5 unisce le stringhe)
@@ -44,9 +46,24 @@ function app(fine, tipo){
     printResults(fine, str3, str5)
 }
 
-app(3, "D")
+function checkNan(){
+    rl.question("Inserisci il valore della variabile \"fine\"", function (answer) {
+        if (isNaN(parseInt(answer))){
+          console.log("Inserire un numero valido")
+          checkNan()
+        } else if (parseInt(answer) < 1){
+            console.log("Inserire un numero positivo")
+            checkNan()
+        }
+        else {
+            return answer
+        }
+    })
+    
+    
+}
 
-  
-//   rl.question("What is your name? ", function (answer) {
-//     console.log(`Oh, so your name is ${answer}`);
-//   });
+//funzione che chiede all'utente i valori delle variabili e controlla il parametro "fine" che sia un numero e che sia maggiore di zero
+// il parametro "tipo" non presenta particolari problemi, il programma funziona anche se il campo è lasciato vuoto
+
+app(checkNan(), "dfd")
